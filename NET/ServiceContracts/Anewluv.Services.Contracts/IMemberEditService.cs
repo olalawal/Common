623217a77a0ac564ed2ea05898323090045f5f30
olalawal <ola_lawal@yahoo.com>
2014-12-26 17:52:53 -0600

    updated

--- NET/ServiceContracts/Anewluv.Services.Contracts/IMemberEditService.cs
+++ NET/ServiceContracts/Anewluv.Services.Contracts/IMemberEditService.cs
@@ -7,6 +7,7 @@ using System.Text;
 using Anewluv.Domain.Data;
 using Anewluv.Domain.Data.ViewModels;
 using System.ServiceModel.Web;
+using System.Threading.Tasks;
 
 namespace Anewluv.Services.Contracts
 {
@@ -24,40 +25,40 @@ namespace Anewluv.Services.Contracts
         //initial profile stuffs
 
         [OperationContract(), FaultContractAttribute(typeof(ServiceFault), Action = "http://Schemas.Testws.Medtox.com")]
-        [WebInvoke(UriTemplate = "/updateprofilevisibilitysettings", ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare)]      
-        bool updateprofilevisibilitysettings(visiblitysetting model);
+        [WebInvoke(UriTemplate = "/updateprofilevisibilitysettings", ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare)]
+        Task<bool> updateprofilevisibilitysettings(visiblitysetting model);
 
         [OperationContract(), FaultContractAttribute(typeof(ServiceFault), Action = "http://Schemas.Testws.Medtox.com")]
         [WebInvoke(UriTemplate = "/getbasicsettingsmodel", ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare)]
-        BasicSettingsModel getbasicsettingsmodel(EditProfileModel editprofilemodel);
+        Task<BasicSettingsModel> getbasicsettingsmodel(EditProfileModel editprofilemodel);
 
         [OperationContract(), FaultContractAttribute(typeof(ServiceFault), Action = "http://Schemas.Testws.Medtox.com")]
         [WebInvoke(UriTemplate = "/getappearancesettingsmodel", ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare)]
-        AppearanceSettingsModel getappearancesettingsmodel(EditProfileModel editprofilemodel);
+        Task<AppearanceSettingsModel> getappearancesettingsmodel(EditProfileModel editprofilemodel);
 
         [OperationContract(), FaultContractAttribute(typeof(ServiceFault), Action = "http://Schemas.Testws.Medtox.com")]
         [WebInvoke(UriTemplate = "/getlifestylesettingsmodel", ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare)]
-        LifeStyleSettingsModel getlifestylesettingsmodel(EditProfileModel editprofilemodel);
+        Task<LifeStyleSettingsModel> getlifestylesettingsmodel(EditProfileModel editprofilemodel);
 
         [OperationContract(), FaultContractAttribute(typeof(ServiceFault), Action = "http://Schemas.Testws.Medtox.com")]
         [WebInvoke(UriTemplate = "/getcharactersettingsmodel", ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare)]
-        CharacterSettingsModel getcharactersettingsmodel(EditProfileModel editprofilemodel);
+          Task<CharacterSettingsModel> getcharactersettingsmodel(EditProfileModel editprofilemodel);
 
         [OperationContract(), FaultContractAttribute(typeof(ServiceFault), Action = "http://Schemas.Testws.Medtox.com")]
         [WebInvoke(UriTemplate = "/membereditbasicsettings", ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare)]   
-        AnewluvMessages membereditbasicsettings(EditProfileModel editprofilemodel);
+          Task<AnewluvMessages> membereditbasicsettings(EditProfileModel editprofilemodel);
 
         [OperationContract(), FaultContractAttribute(typeof(ServiceFault), Action = "http://Schemas.Testws.Medtox.com")]
         [WebInvoke(UriTemplate = "/membereditappearancesettings", ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare)]   
-        AnewluvMessages membereditappearancesettings(EditProfileModel editprofilemodel);
+          Task<AnewluvMessages> membereditappearancesettings(EditProfileModel editprofilemodel);
 
         [OperationContract(), FaultContractAttribute(typeof(ServiceFault), Action = "http://Schemas.Testws.Medtox.com")]
         [WebInvoke(UriTemplate = "/membereditlifestylesettings", ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare)]   
-        AnewluvMessages membereditlifestylesettings(EditProfileModel editprofilemodel);
+          Task<AnewluvMessages> membereditlifestylesettings(EditProfileModel editprofilemodel);
 
         [OperationContract(), FaultContractAttribute(typeof(ServiceFault), Action = "http://Schemas.Testws.Medtox.com")]
         [WebInvoke(UriTemplate = "/membereditcharactersettings", ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare)]   
-        AnewluvMessages membereditcharactersettings(EditProfileModel editprofilemodel);
+          Task<AnewluvMessages> membereditcharactersettings(EditProfileModel editprofilemodel);
 
     }
 }
