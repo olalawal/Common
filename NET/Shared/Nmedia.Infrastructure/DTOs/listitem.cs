6248630ffdacfee344118be2491ed3a1a31410b8
olalawal <ola_lawal@yahoo.com>
2015-01-24 01:20:49 -0600

    updated

---
+++ NET/Shared/Nmedia.Infrastructure/DTOs/listitem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nmedia.Infrastructure.DTOs
+{    
+
+        [DataContract]
+        public class listitem
+        {          
+            [DataMember]
+            public int id { get; set; }
+            [DataMember]
+            public bool selected { get; set; }
+            [DataMember]
+            public string description { get; set; }           
+        }
+    
+}
